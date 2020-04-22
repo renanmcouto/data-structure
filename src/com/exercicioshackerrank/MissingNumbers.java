0eadf01d23830676863caa0e764ebcea12793139
Lucas <lucasmarinds@gmail.com>
2020-04-22 01:06:55 -0300

    Quase consegui quem sabe na proxima

--- src/com/exercicioshackerrank/MissingNumbers.java
+++ src/com/exercicioshackerrank/MissingNumbers.java
@@ -10,46 +10,24 @@ public class MissingNumbers {
         int jj=0;
         ArrayList<Integer> numerosFaltando = new ArrayList();
 
-        //int valorPrimeiroArray = scanner.nextInt();
-        int[] arr = new int[10];
+        int valorPrimeiroArray = scanner.nextInt();
+        int[] arr = new int[valorPrimeiroArray];
 
-        arr[0] = 203;
-        arr[1] = 204;
-        arr[2] = 205;
-        arr[3] = 206;
-        arr[4] = 207;
-        arr[5] = 208;
-        arr[6] = 203;
-        arr[7] = 204;
-        arr[8] = 205;
-        arr[9] = 206;
 
-//        for (int i = 0; i < valorPrimeiroArray; i++) {
-//            arr[i] = scanner.nextInt();
-//        }
+        for (int i = 0; i < valorPrimeiroArray; i++) {
+            arr[i] = scanner.nextInt();
+        }
 
-        //int valorSegundoArray = scanner.nextInt();
-        int[] brr = new int[13];
+        int valorSegundoArray = scanner.nextInt();
+        int[] brr = new int[valorSegundoArray];
 
-        brr[0] = 203;
-        brr[1] = 204;
-        brr[2] = 204;
-        brr[3] = 205;
-        brr[4] = 206;
-        brr[5] = 207;
-        brr[6] = 205;
-        brr[7] = 208;
-        brr[8] = 203;
-        brr[9] = 206;
-        brr[10] = 205;
-        brr[11] = 206;
-        brr[12] = 204;
+        for (int i = 0; i < valorSegundoArray; i++) {
+            brr[i] = scanner.nextInt();
+        }
 
-//        for (int i = 0; i < valorSegundoArray; i++) {
-//            brr[i] = scanner.nextInt();
-//        }
+        scanner.close();
 
-        //Pega os numeros que faltam dentro do outro array.
+        //Faz a inserção dentro da lista.
         for (int i = 0; i < arr.length; i++) {
             for(int j = jj; j < brr.length; j++){
                     if(brr[j] == arr[i]){
@@ -62,7 +40,7 @@ public class MissingNumbers {
             }
         }
 
-        //Remove Numeros Duplicados.
+        //Remove duplicações dentro da lista.
         for(int i=0; i<numerosFaltando.size(); i++){
             for(int j=1; j<numerosFaltando.size(); j++){
                 if(numerosFaltando.get(i).equals(numerosFaltando.get(j))){
@@ -73,7 +51,8 @@ public class MissingNumbers {
                 }
             }
         }
-        //Ordenação da Lista.
+
+        //Organiza a Lista por ordem.
         for(int i=0; i<numerosFaltando.size()-1; i++){
             if(!(numerosFaltando.get(i) < numerosFaltando.get(i+1))){
                 int aux = numerosFaltando.get(i);
@@ -81,7 +60,13 @@ public class MissingNumbers {
                 numerosFaltando.add(aux);
             }
         }
-
-        System.out.println(numerosFaltando.toString());
+        //Printa o Resultado
+        for(int i=0; i<numerosFaltando.size(); i++){
+            if(i < numerosFaltando.size()-1) {
+                System.out.print(numerosFaltando.get(i) + " ");
+            }else{
+                System.out.print(numerosFaltando.get(i));
+            }
+        }
     }
 }
